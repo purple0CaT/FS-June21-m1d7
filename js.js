fe6fe4c45b077d04b37a3cd93f673a974a323602
Istvan <steven.ford.j@gmail.com>
2021-06-29 15:03:31 +0100

    Added CSS class to Amazon link
    My links is a whole tab, but it still working :D

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 const header1 = document.querySelector('h1')
 const backGround = document.querySelector('section')
 const footAdrs = document.querySelector('.info p')
+const amazonLink = document.querySelectorAll('.table a')
 
 // H1 function changer
 function changeText(){
@@ -13,4 +14,11 @@ function changeBackCol(){
 // Footer Address
 function footerAddress(){
 footAdrs.innerText = "Wales, Mountain Street"
+}
+
+// Add CSS class to Amazon link
+
+function addCssCls () {
+for ( let tabs of amazonLink){
+    tabs.style.backgroundColor = 'rgb(230, 251, 255)'}
 }
