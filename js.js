7f7e1894812c375e7a6321eeedf57eec178c3d0f
Istvan <steven.ford.j@gmail.com>
2021-06-29 14:54:46 +0100

    Did the background color function
    Its changing not exactly the body, only the section (because section is like 90% page :d)

--- js.js
+++ js.js
@@ -1,6 +1,10 @@
 const header1 = document.querySelector('h1')
-
+const backGround = document.querySelector('section')
 // H1 function changer
 function changeText(){
     header1.style.color = "Gray"
+}
+//  BackGround color changer
+function changeBackCol(){
+    backGround.style.backgroundColor = "#fff"
 }
