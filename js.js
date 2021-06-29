9bc009f2477ca1f2027ed8ac2ecb37765a5b2a7c
Istvan <steven.ford.j@gmail.com>
2021-06-29 15:11:18 +0100

    Solved Visibility function and a bit fixed HTML

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ const header1 = document.querySelector('h1')
 const backGround = document.querySelector('section')
 const footAdrs = document.querySelector('.info p')
 const amazonLink = document.querySelectorAll('.table a')
-
+const imgLink= document.querySelectorAll('a img')
 // H1 function changer
 function changeText(){
     header1.style.color = "Gray"
@@ -21,4 +21,11 @@ footAdrs.innerText = "Wales, Mountain Street"
 function addCssCls () {
 for ( let tabs of amazonLink){
     tabs.classList.toggle("linkClass")}
+}
+
+// Visibility for IMG
+
+function visImg() {
+    for ( let tabs of imgLink){
+        tabs.classList.toggle("visibImg")}
 }
